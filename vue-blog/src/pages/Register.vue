6016aa46e1edc263a76dac608c189da5632ce25f
TAVnotDove <slavei.dikovski123@gmail.com>
2023-12-10 19:28:03 +0200

    installed axios and added register functionality

--- vue-blog/src/pages/Register.vue
+++ vue-blog/src/pages/Register.vue
@@ -1,46 +1,82 @@
 <script>
-    import { useVuelidate } from '@vuelidate/core'
-    import { required, email } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+import { register } from '../dataProviders/auth'
 
-    export default {
-        setup() {
-            return { v$: useVuelidate() }
-        },
-        validations() {
-            return {
-                data: {
-                    username: { required },
-                    email: { required, email },
-                    password: { required },
-                    confirmPassword: { required },
+export default {
+    data() {
+        return {
+            formData: {
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            },
+            submitError: ''
+        }
+    },
+    setup() {
+        return { v$: useVuelidate() }
+    },
+    validations() {
+        return {
+            formData: {
+                username: { required },
+                email: { required, email },
+                password: { required },
+                confirmPassword: { required },
+            }
+        }
+    },
+    methods: {
+        async submitHandler() {
+            const isValid = await this.v$.$validate()
+
+            if (isValid) {
+                const { username, email, password } = this.formData
+
+                const res = await register({
+                    username,
+                    email,
+                    password
+                })
+
+                if (!res.message) {
+                    localStorage.setItem('user-data', JSON.stringify(res.data))
+                } else {
+                    this.submitError = res.message
                 }
             }
         }
     }
+}
 </script>
 
 <template>
-    <form>
-        <div>
-            <label>Email</label>
-            <input>
-        </div>
+    <form @submit.prevent="submitHandler">
         <div>
             <label>Username</label>
-            <input>
+            <input v-model="formData.username">
+        </div>
+        <div>
+            <label>Email</label>
+            <input v-model="formData.email">
         </div>
         <div>
             <label>Password</label>
-            <input>
+            <input v-model="formData.password">
         </div>
         <div>
             <label>Confirm password</label>
-            <input>
+            <input v-model="formData.confirmPassword">
         </div>
         <button>Register</button>
+        <p class="error">{{ submitError }}</p>
     </form>
 </template>
 
 <style scoped>
-
+.error {
+    color: red;
+}
 </style>
