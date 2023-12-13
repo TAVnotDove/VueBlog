f8706595c181e88724971e3f2a0f30676185453d
TAVnotDove <slavei.dikovski123@gmail.com>
2023-12-13 23:35:28 +0200

    added route guards

--- vue-blog/src/routes/index.js
+++ vue-blog/src/routes/index.js
@@ -8,15 +8,59 @@ import PostDetails from "../pages/PostDetails.vue";
 import CreatePost from "../pages/CreatePost.vue";
 import EditPost from "../pages/EditPost.vue";
 
+function authGuard(to, from) {
+  if (!localStorage.getItem("user-data") && to.path !== "/login") {
+    return { path: "/login" };
+  }
+}
+
+function notAuthGuard(to, from) {
+  if (localStorage.getItem("user-data") && to.path !== "/posts") {
+    return { path: "/posts" };
+  }
+}
+
 const routes = [
-  { path: "/", component: Home },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/profile", component: Profile },
-  { path: "/posts", component: Posts },
-  { path: "/posts/:postId", component: PostDetails },
-  { path: "/posts/create", component: CreatePost },
-  { path: "/posts/:postId/edit", component: EditPost },
+  {
+    path: "/",
+    component: Home,
+    beforeEnter: (to, from) => notAuthGuard(to, from),
+  },
+  {
+    path: "/login",
+    component: Login,
+    beforeEnter: (to, from) => notAuthGuard(to, from),
+  },
+  {
+    path: "/register",
+    component: Register,
+    beforeEnter: (to, from) => notAuthGuard(to, from),
+  },
+  {
+    path: "/profile",
+    component: Profile,
+    beforeEnter: (to, from) => authGuard(to, from),
+  },
+  {
+    path: "/posts",
+    component: Posts,
+    beforeEnter: (to, from) => authGuard(to, from),
+  },
+  {
+    path: "/posts/:postId",
+    component: PostDetails,
+    beforeEnter: (to, from) => authGuard(to, from),
+  },
+  {
+    path: "/posts/create",
+    component: CreatePost,
+    beforeEnter: (to, from) => authGuard(to, from),
+  },
+  {
+    path: "/posts/:postId/edit",
+    component: EditPost,
+    beforeEnter: (to, from) => authGuard(to, from),
+  },
 ];
 
 const router = createRouter({
