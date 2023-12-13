997432d5bfe6f0bbd086c46e7eccab1f47a6797b
TAVnotDove <slavei.dikovski123@gmail.com>
2023-12-13 22:36:50 +0200

    added content to profile page

--- vue-blog/src/pages/Profile.vue
+++ vue-blog/src/pages/Profile.vue
@@ -1,11 +1,29 @@
 <script>
-export default {
+import useUserStore from "../store/userStore";
+import { mapState } from "pinia";
 
+export default {
+    data() {
+        return {
+            user: {}
+        }
+    },
+    computed: {
+        ...mapState(useUserStore, ['userData'])
+    },
+    mounted() {
+        this.user = { ...this.userData.user }
+    }
 }
 </script>
 
 <template>
     <h1>Profile</h1>
+    <div>
+        <p>Username - {{ user.username }}</p>
+        <p>Email - {{ user.email }}</p>
+        <p>Created - {{ new Date(user.createDate).toString().substring(4, 24) }}</p>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
